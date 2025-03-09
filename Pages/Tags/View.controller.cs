638899d181f86c16b4317de9c3fc9abd75333f37
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2025-03-09 22:15:12 +0700

    Add subscribe/unsubscribe from tags

--- Pages/Tags/View.controller.cs
+++ Pages/Tags/View.controller.cs
@@ -7,12 +7,22 @@ using alma.Services;
 
 namespace alma.Pages.Tags;
 
-public class ViewTagModel(DatabaseContext database) : PageModel {
+public class ViewTagModel(DatabaseContext database, ISessionService sessionService) : PageModel {
     private readonly DatabaseContext _database = database;
+    private readonly ISessionService _sessionService = sessionService;
 
+    public new User? User { get; set; } = default!;
     public Tag Tag { get; set; } = default!;
 
     public async Task<IActionResult> OnGetAsync(string id) {
+        var user = await _sessionService.GetUserAsync(HttpContext.Request.Cookies["session"] ?? "");
+
+        if (user is not null) {
+            await _database.Entry(user).Collection(u => u.FollowedTags).LoadAsync();
+        }
+
+        User = user;
+
         var tag = await _database.Tag.Include(t => t.Events).Include(t => t.Followers).FirstOrDefaultAsync(t => t.Id == id);
         if (tag == null) {
             return NotFound();
